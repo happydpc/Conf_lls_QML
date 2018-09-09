d4b1505ae49195c92147285c425755b50158f6d0
Your Name <you@example.com>
2018-09-09 20:30:36 +0300

    up

--- command/commandController.h
+++ command/commandController.h
@@ -21,7 +21,7 @@ public:
         // bytearray for command buf
         QByteArray commandOptionData;
         // buffer for transmit settings, firmware and other data
-        QList<QPair<QString,QByteArray>>commandOptionArg;
+        QList<int>commandOptionArg;
     }sCommandData;
 
     bool addCommandToStack(sCommandData commandData);
