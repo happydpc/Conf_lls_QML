257511250286a4c80f1aa6028402e162160355bf
Your Name <you@example.com>
2018-09-18 23:37:15 +0300

    up

--- device/Progress_TMK24/calibrate/calibrate.cpp
+++ device/Progress_TMK24/calibrate/calibrate.cpp
@@ -4,3 +4,27 @@ Calibrate::Calibrate(QObject *parent) : QObject(parent)
 {
 
 }
+
+void Calibrate::reset() {
+
+}
+bool Calibrate::addDevice(QString caption, QString uniqNameId) {
+
+}
+void Calibrate::removeDevice(int index) {
+
+}
+
+bool Calibrate::addStep(uint32_t valueLiters, QList<uint32_t>valueCntDevs) {
+
+}
+bool Calibrate::changeStep(int index, uint32_t valueLiters, QList<uint32_t>valueCntDevs) {
+
+}
+void Calibrate::removeStep(int index) {
+
+}
+
+QString Calibrate::getLastError() {
+
+}
--- device/Progress_TMK24/calibrate/calibrate.h
+++ device/Progress_TMK24/calibrate/calibrate.h
@@ -2,6 +2,9 @@
 #define CALIBRATE_H
 
 #include <QObject>
+#include <QList>
+#include <QPair>
+#include <QString>
 
 class Calibrate : public QObject
 {
@@ -9,9 +12,31 @@ class Calibrate : public QObject
 public:
     explicit Calibrate(QObject *parent = nullptr);
 
+public slots:
+    void reset();
+
+    bool addDevice(QString caption, QString uniqNameId);
+    void removeDevice(int index);
+
+    bool addStep(uint32_t valueLiters, QList<uint32_t>valueCntDevs);
+    bool changeStep(int index, uint32_t valueLiters, QList<uint32_t>valueCntDevs);
+    void removeStep(int index);
+
+    QString getLastError();
+
 signals:
 
-public slots:
+private:
+
+    typedef struct {
+        uint32_t valueCnt;
+        uint32_t valueLiters;
+    }sValues;
+
+    QList<QPair<QString, sValues>>devList;
+
+    QString lastError;
+
 };
 
 #endif // CALIBRATE_H
